878ef1231a9066f525fc1d5535a8c5675dc15f94
Ulrich Barnstedt <ulrich.barnstedt@gmail.com>
2021-10-27 01:05:06 +0200

    Implement button functionality and push icons

--- src/components/tree/RepoVisual.js
+++ src/components/tree/RepoVisual.js
@@ -1,5 +1,5 @@
 import Tree from "react-animated-tree";
-import {Panel, TagGroup, Tag, IconButton} from "rsuite";
+import {Panel, TagGroup, Tag, IconButton, toaster, Notification} from "rsuite";
 import BranchIcon from '@rsuite/icons/Branch';
 import CodeIcon from '@rsuite/icons/Code';
 import PinIcon from '@rsuite/icons/Pin';
@@ -8,6 +8,22 @@ import styles from "../../styles/global.module.css"
 import CopyIcon from '@rsuite/icons/Copy';
 import FileDownloadIcon from '@rsuite/icons/FileDownload';
 import ProjectIcon from '@rsuite/icons/Project';
+
+const copyCloneURL = (data) => {
+    return () => {
+        navigator.clipboard.writeText(data.clone_url);
+        toaster.push(
+            <Notification type="success" header="Copied URL to clipboard"/>,
+            {
+                placement: 'topEnd'
+            }
+        )
+    }
+}
+
+const downloadZIP = (data) => {
+
+}
 
 const RepoVisual = ({data, displayName}) => {
     return <Tree type="[REPO]" content={displayName}>
@@ -44,6 +60,7 @@ const RepoVisual = ({data, displayName}) => {
                                 placement="left"
                                 icon={<CopyIcon/>}
                                 appearance="ghost"
+                                onClick={copyCloneURL(data)}
                                 size="xs">
                         Clone
                     </IconButton>
@@ -51,6 +68,7 @@ const RepoVisual = ({data, displayName}) => {
                                 appearance="ghost"
                                 placement="left"
                                 icon={<ProjectIcon/>}
+                                href={data.html_url}
                                 size="xs">
                         View
                     </IconButton>
@@ -58,6 +76,9 @@ const RepoVisual = ({data, displayName}) => {
                                 appearance="ghost"
                                 placement="left"
                                 icon={<FileDownloadIcon/>}
+                                onClick={downloadZIP(data)}
+                                target="_blank"
+                                href={data.archive_url}
                                 size="xs">
                         Download
                     </IconButton>
